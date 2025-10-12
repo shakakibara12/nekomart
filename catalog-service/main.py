6a72bb9281ff0ece2f2406d00e25bee9edfdcfca
shakakibara <aryan.1843@disroot.org>
2025-10-13 05:20:26 +0530

    Add mock database

--- catalog-service/main.py
+++ catalog-service/main.py
@@ -1,6 +1,7 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI(title="NekoMart Catalog Service")
 
@@ -12,6 +13,39 @@ class Product(BaseModel):
     category: str  # e.g., Anime, Food, Beauty
     price: float
     image_url: str
+
+
+# Mock Database
+PRODUCTS_DB = [
+    Product(
+        id=1,
+        name="Demon Slayer Figure",
+        category="Anime",
+        price=49.99,
+        image_url="/img/naruto.jpg",
+    ),
+    Product(
+        id=2,
+        name="Matcha KitKat",
+        category="Food",
+        price=5.50,
+        image_url="/img/kitkat.jpg",
+    ),
+    Product(
+        id=3,
+        name="Super Mario Plush",
+        category="Games",
+        price=25.00,
+        image_url="/img/mario.jpg",
+    ),
+    Product(
+        id=4,
+        name="K-beauty Serum",
+        category="Beauty",
+        price=30.00,
+        image_url="/img/serum.jpg",
+    ),
+]
 
 
 @app.get("/")
